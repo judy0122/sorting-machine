5be9bb770c4a8e5b7ed3457b2c1395b177e4d425
jiwon <jiwon.park2@solarconnect.kr>
2021-07-28 09:19:30 +0900

    initial setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from "react";
+import SortingContainer from "./components/sorting/SortingContainer";
 
 function App() {
   return (
     <div className="App">
-      
+      <SortingContainer />
     </div>
   );
 }
