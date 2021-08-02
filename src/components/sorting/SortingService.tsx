56c5f890c4ac4401e08c3cd119699f350fc7e1bb
jiwon <jiwon.park2@solarconnect.kr>
2021-08-02 16:38:09 +0900

    sort javascript > 병합정렬 알고리즘 변경

--- src/components/sorting/SortingService.tsx
+++ src/components/sorting/SortingService.tsx
@@ -4,14 +4,49 @@ const useSorting = () => {
   const [ascending, setAscending] = React.useState<string>("-");
   const [descending, setDescending] = React.useState<string>("-");
 
+  // 병합정렬로 구현하였습니다.
+  // 오름차순
+  const sortAscendArray = (left: number[], right: number[]): number[] => {
+    const result: any[] = [];
+    while (left.length !== 0 && right.length !== 0) {
+      left[0] <= right[0]
+        ? result.push(left.shift())
+        : result.push(right.shift());
+    }
+
+    return [...result, ...left, ...right];
+  };
+
+  // 내림차순
+  const sortDescendArray = (left: number[], right: number[]): number[] => {
+    const result: any[] = [];
+    while (left.length !== 0 && right.length !== 0) {
+      left[0] >= right[0]
+        ? result.push(left.shift())
+        : result.push(right.shift());
+    }
+
+    return [...result, ...left, ...right];
+  };
+
+  const sliceArray = (array: number[], isAscend: boolean): number[] => {
+    if (array.length === 1) return array;
+    const middleIdx = Math.floor(array.length / 2);
+    const left: number[] = array.slice(0, middleIdx);
+    const right: number[] = array.slice(middleIdx);
+
+    return isAscend
+      ? sortAscendArray(sliceArray(left, isAscend), sliceArray(right, isAscend))
+      : sortDescendArray(
+          sliceArray(left, isAscend),
+          sliceArray(right, isAscend)
+        );
+  };
+
   const startSorting = (value: string) => {
     const values: number[] = value.split(",").map((x) => parseInt(x));
-    const newAscending = values
-      .sort((a: number, b: number): number => {
-        return b - a;
-      })
-      .toString();
-    const newDescending = values.reverse().toString();
+    const newAscending = sliceArray(values, true).toString();
+    const newDescending = sliceArray(values, false).toString();
     setAscending(newAscending);
     setTimeout(() => {
       setDescending(newDescending);
